c6d39533b9fbb1463b3e56fa19d28b5db8a4a46b
Mike Wild <wildconsultingltd@googlemail.com>
2022-08-08 11:01:34 +0100

    Add extensions class for page title

---
+++ src/Sfa.Tl.Service.Home/Extensions/PageExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Sfa.Tl.Service.Home.Extensions;
+
+public static class PageExtensions
+{
+    private const string ServiceName = "T Levels Service Home";
+    private const string GovUk = "GOV.UK";
+
+    public static string GenerateTitle(
+        string? title,
+        bool isValid)
+    {
+        var ignoreTitle = title == ServiceName;
+        const string titleSuffix = $"{ServiceName} - {GovUk}";
+
+        var formattedTitle = new StringBuilder();
+        if (!isValid) formattedTitle.Append("Error: ");
+        if (string.IsNullOrEmpty(title))
+        {
+            return formattedTitle.Append(titleSuffix).ToString();
+        }
+
+        if (!ignoreTitle) formattedTitle.Append($"{title} - ");
+        formattedTitle.Append(titleSuffix);
+
+        return formattedTitle.ToString();
+    }
+}
