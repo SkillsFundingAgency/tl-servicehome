bc03edd1b525e9d3e65e5659c27716b7ad2de6aa
ashleyanderton <ashley.j.anderton@gmail.com>
2022-08-09 10:30:07 +0100

    Cookies

---
+++ src/Sfa.Tl.Service.Home/Pages/Help/Cookies.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sfa.Tl.Service.Home.Pages;
+
+public class CookiesModel : PageModel
+{
+    private readonly ILogger<CookiesModel> _logger;
+
+    public CookiesModel(ILogger<CookiesModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
