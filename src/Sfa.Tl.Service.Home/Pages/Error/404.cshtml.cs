0a2f1ce60c1be6ce65f4ee6bb9393d9094a82b98
ashleyanderton <ashley.j.anderton@gmail.com>
2022-08-09 12:28:41 +0100

    Error page added

---
+++ src/Sfa.Tl.Service.Home/Pages/Error/404.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sfa.Tl.Service.Home.Pages;
+
+public class Error404Model : PageModel
+{
+    private readonly ILogger<Error404Model> _logger;
+
+    public Error404Model(ILogger<Error404Model> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
