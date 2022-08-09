5bb50a53a0ea4501ecac35c41967374498729a7f
Mike Wild <wildconsultingltd@googlemail.com>
2022-08-09 12:12:37 +0100

    Fix cookis namespace and remove unused code

--- src/Sfa.Tl.Service.Home/Pages/Help/Cookies.cshtml.cs
+++ src/Sfa.Tl.Service.Home/Pages/Help/Cookies.cshtml.cs
@@ -1,18 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Sfa.Tl.Service.Home.Pages;
+namespace Sfa.Tl.Service.Home.Pages.Help;
 
 public class CookiesModel : PageModel
 {
-    private readonly ILogger<CookiesModel> _logger;
-
-    public CookiesModel(ILogger<CookiesModel> logger)
-    {
-        _logger = logger;
-    }
-
     public void OnGet()
     {
-
     }
 }
