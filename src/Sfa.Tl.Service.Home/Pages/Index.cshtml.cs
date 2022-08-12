4131ec5adcecd9a05875a49a1b7811cde5a6d345
Mike Wild <wildconsultingltd@googlemail.com>
2022-08-12 13:49:40 +0100

    Refactoring

--- src/Sfa.Tl.Service.Home/Pages/Index.cshtml.cs
+++ src/Sfa.Tl.Service.Home/Pages/Index.cshtml.cs
@@ -8,29 +8,28 @@ public class IndexModel : PageModel
 {
     private readonly LinkSettings _linkSettings;
     private readonly ILogger<IndexModel> _logger;
-
-    public string? EmployerSupportSiteUrl => 
-        _linkSettings.EmployerSupportSiteUrl;
-
-    public string? ProviderSupportSiteUrl =>
-        _linkSettings.ProviderSupportSiteUrl;
     
-    public string? ProviderDataSiteUrl =>
-        _linkSettings.ProviderDataSiteUrl;
+    public string? EmployerSupportSiteUrl { get; private set; }
 
-    public string? ResultsAndCertificationsSiteUrl =>
-        _linkSettings.ResultsAndCertificationsSiteUrl;
+    public string? ProviderSupportSiteUrl { get; private set; }
+
+    public string? ProviderDataSiteUrl { get; private set; }
+
+    public string? ResultsAndCertificationsSiteUrl { get; private set; }
 
     public IndexModel(
         IOptions<LinkSettings> linkSettings, 
         ILogger<IndexModel> logger)
     {
-        _linkSettings = linkSettings?.Value ?? throw new ArgumentNullException(nameof(linkSettings));
-        _logger = logger;
+        _linkSettings = linkSettings.Value ?? throw new ArgumentNullException(nameof(linkSettings));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
     public void OnGet()
     {
-
+        EmployerSupportSiteUrl = _linkSettings.EmployerSupportSiteUrl;
+        ProviderSupportSiteUrl = _linkSettings.ProviderSupportSiteUrl;
+        ProviderDataSiteUrl = _linkSettings.ProviderDataSiteUrl;
+        ResultsAndCertificationsSiteUrl = _linkSettings.ResultsAndCertificationsSiteUrl;
     }
 }
